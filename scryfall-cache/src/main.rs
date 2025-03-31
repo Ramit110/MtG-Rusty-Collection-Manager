c70b504c5e32be2f390bec9d025241510fc83b2a
ramit110 <Ramit110@users.noreply.github.com>
2025-03-31 18:24:17 +0100

    formatting and improving the rust

--- scryfall-cache/src/main.rs
+++ scryfall-cache/src/main.rs
@@ -1,6 +1,6 @@
 use reqwest::header::{ACCEPT, USER_AGENT};
 use serde::{Serialize, Deserialize};
-use std::fs::{File, create_dir};
+use std::fs::{File::create, create_dir};
 use std::io::Write;
 
 #[derive(Deserialize, Serialize)]
@@ -28,38 +28,36 @@ struct ScryfallBulkDataResponse {
 #[tokio::main]
 async fn main() {
     println!("Getting Data from Scryfall!");
-    let client = reqwest::Client::new();
-    let scryfall_data = client.get(String::from("https://api.scryfall.com/bulk-data"))
+    let scryfall_data = reqwest::Client::new()
+        .get(String::from("https://api.scryfall.com/bulk-data"))
         .header(USER_AGENT, "Scryfall Cacher")
         .header(ACCEPT, "application/json")
-        .send()
-        .await.expect("API Error")
-        .json::<ScryfallBulkDataResponse>()
-        .await.expect("JSON Error");
+        .send().await.expect("API Error")
+        .json::<ScryfallBulkDataResponse>().await.expect("JSON Parse Error")
+        .data;
     println!("Got data from Scryfall!");
 
     println!("Creating initial folder!");
-    create_dir("/initial").ok();
+    create_dir("/initial").expect("Error Making Initial Folder");
     println!("Created initial folder!");
 
     println!("Getting the JSON files!");
-    let mut handles = vec![];
-    for dataset in scryfall_data.data {
+    let handles = vec![];
+    for dataset in scryfall_data {
         let handle = tokio::spawn(async move {
             println!("starting download of: {0}", dataset.r#type);
-            let client = reqwest::Client::new();
-            let mut output_file = File::create(format!("/initial/{0}.json", dataset.r#type).as_str())
+            let mut output_file = create(format!("/initial/{0}.json", dataset.r#type).as_str())
                 .expect(format!("Error making file for {0}!", dataset.r#type).as_str());
         
-            let scryfall_data = client.get(&dataset.download_uri)
-                .send().await.expect("Error API call failed")
-                .text().await.expect("File Error");
+            let scryfall_data_files = reqwest::Client::new()
+                .get(&dataset.download_uri)
+                .send().await.expect(format!("Error Getting Data File for {0}", dataset.r#type))
+                .text().await.expect(format!("Error Parsing File For {0}", dataset.r#type));
         
-            output_file.write(scryfall_data.as_bytes())
+            output_file.write(scryfall_data_files.as_bytes())
                 .expect(format!("Error writing to file {0}!", dataset.r#type).as_str());
         
             println!("Finished Downloading: {0}", dataset.r#type);
-        
             Ok("Success!")
         });
         handles.push(handle);
